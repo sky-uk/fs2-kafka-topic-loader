c01bf57b58961acc6d5d3cc16b611a71c0be881d
Ben Carter <ben.carter@sky.uk>
2023-05-24 23:42:36 +0100

    Extract running test function

--- it/src/test/scala/integration/LoadExampleIntSpec.scala
+++ it/src/test/scala/integration/LoadExampleIntSpec.scala
@@ -20,29 +20,13 @@ class LoadExampleIntSpec extends WordSpecBase with Eventually {
   "LoadExample" should {
     "load previously seen messages into the store before processing new messages" in new TestContext {
 
-      val store: IO[Ref[IO, List[String]]] = Ref[IO].of(List.empty)
-
-      implicit val logger: Logger[IO] = Slf4jFactory.create[IO].getLogger
-
       withRunningKafka {
 
         publishToKafka(inputTopic, "key1", "value1")
 
-        {
-          for {
-            store   <- store
-            example1 =
-              LoadExample.kafka[IO](
-                topics = topics,
-                outputTopic = outputTopic,
-                consumerSettings = consumerSettings,
-                producerSettings = producerSettings,
-                store = store
-              )
-            _       <- example1.stream.interruptAfter(timeout).compile.drain
-            stored  <- store.get
-          } yield stored
-        }.unsafeRunSync() should contain theSameElementsInOrderAs List("value1")
+        runApp { result =>
+          result should contain theSameElementsInOrderAs List("value1")
+        }
 
         eventually {
           consumeFirstStringMessageFrom(outputTopic, autoCommit = true) shouldBe "value1"
@@ -50,53 +34,57 @@ class LoadExampleIntSpec extends WordSpecBase with Eventually {
 
         publishToKafka(inputTopic, "key2", "value2")
 
-        {
-          for {
-            store   <- store
-            example1 =
-              LoadExample.kafka[IO](
-                topics = topics,
-                outputTopic = outputTopic,
-                consumerSettings = consumerSettings,
-                producerSettings = producerSettings,
-                store = store
-              )
-            _       <- example1.stream.interruptAfter(timeout).compile.drain
-            stored  <- store.get
-          } yield stored
-        }.unsafeRunSync() should contain theSameElementsInOrderAs List("value1", "value2")
+        runApp { result =>
+          result should contain theSameElementsInOrderAs List("value1", "value2")
+        }
 
         eventually {
           consumeFirstStringMessageFrom(outputTopic, autoCommit = true) shouldBe "value2"
         }
       }
-
     }
   }
 
   private trait TestContext extends EmbeddedKafka {
-    implicit lazy val kafkaConfig: EmbeddedKafkaConfig =
-      EmbeddedKafkaConfig(kafkaPort = RandomPort(), zooKeeperPort = RandomPort(), Map("log.roll.ms" -> "10"))
-
-    val timeout = 10.seconds
-
-    val inputTopic = "test-topic-1"
-    val topics     = NonEmptyList.one(inputTopic)
-
+    val inputTopic  = "test-topic-1"
     val outputTopic = "output-topic-1"
 
-    val groupId = "load-example-consumer-group"
+    private val store: IO[Ref[IO, List[String]]] = Ref[IO].of(List.empty)
 
-    val consumerSettings: ConsumerSettings[IO, String, String] =
+    private implicit val logger: Logger[IO] = Slf4jFactory.create[IO].getLogger
+
+    private implicit lazy val kafkaConfig: EmbeddedKafkaConfig =
+      EmbeddedKafkaConfig(kafkaPort = RandomPort(), zooKeeperPort = RandomPort(), Map("log.roll.ms" -> "10"))
+
+    private val timeout = 10.seconds
+
+    private val consumerSettings: ConsumerSettings[IO, String, String] =
       ConsumerSettings[IO, String, String]
         .withBootstrapServers(s"localhost:${kafkaConfig.kafkaPort}")
         .withAutoOffsetReset(AutoOffsetReset.Earliest)
-        .withGroupId(groupId)
+        .withGroupId("load-example-consumer-group")
 
-    val producerSettings: ProducerSettings[IO, String, String] =
+    private val producerSettings: ProducerSettings[IO, String, String] =
       ProducerSettings[IO, String, String]
         .withBootstrapServers(s"localhost:${kafkaConfig.kafkaPort}")
 
-  }
+    def runApp[T](f: List[String] => T): T = {
+      val storeState = for {
+        store   <- store
+        example1 =
+          LoadExample.kafka[IO](
+            topics = NonEmptyList.one(inputTopic),
+            outputTopic = outputTopic,
+            consumerSettings = consumerSettings,
+            producerSettings = producerSettings,
+            store = store
+          )
+        _       <- example1.stream.interruptAfter(timeout).compile.drain
+        stored  <- store.get
+      } yield stored
 
+      f(storeState.unsafeRunSync())
+    }
+
+  }
 }
