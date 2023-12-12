e175e97d80272bc73eb474ae7287c39470ae3617
Scala Steward <88037287+disco-scala-steward@users.noreply.github.com>
2023-12-12 14:22:56 +0000

    Update embedded-kafka from 3.6.0 to 3.6.1 (#51)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,7 +14,7 @@ object Dependencies {
     lazy val kafka = "com.github.fd4s" %% "fs2-kafka" % "3.2.0"
   }
 
-  lazy val embeddedKafka     = "io.github.embeddedkafka" %% "embedded-kafka"                % "3.6.0"  % Test
+  lazy val embeddedKafka     = "io.github.embeddedkafka" %% "embedded-kafka"                % "3.6.1"  % Test
   lazy val scalaTest         = "org.scalatest"           %% "scalatest"                     % "3.2.17" % Test
   lazy val catsEffectTesting = "org.typelevel"           %% "cats-effect-testing-scalatest" % "1.5.0"  % Test
 
