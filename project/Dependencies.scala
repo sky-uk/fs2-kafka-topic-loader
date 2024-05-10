8f881ba0416b50a107915df64c9d7946a7d38298
Scala Steward <88037287+disco-scala-steward@users.noreply.github.com>
2024-05-10 09:03:44 +0100

    Update fs2-core from 3.10.0 to 3.10.2 (#79)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -10,7 +10,7 @@ object Dependencies {
   }
 
   object Fs2 {
-    lazy val core  = "co.fs2"          %% "fs2-core"  % "3.10.0"
+    lazy val core  = "co.fs2"          %% "fs2-core"  % "3.10.2"
     lazy val kafka = "com.github.fd4s" %% "fs2-kafka" % "3.3.1"
   }
 
