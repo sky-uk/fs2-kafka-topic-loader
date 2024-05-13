58d37179afebd8715feac4519bdcc143191a3c0e
Scala Steward <disco-scala-steward-github-user@skyglobal.onmicrosoft.com>
2024-05-13 07:22:07 +0000

    Update fs2-kafka from 3.3.1 to 3.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -11,7 +11,7 @@ object Dependencies {
 
   object Fs2 {
     lazy val core  = "co.fs2"          %% "fs2-core"  % "3.11.0"
-    lazy val kafka = "com.github.fd4s" %% "fs2-kafka" % "3.3.1"
+    lazy val kafka = "com.github.fd4s" %% "fs2-kafka" % "3.5.1"
   }
 
   lazy val testContainersKafka = "org.testcontainers" % "kafka"                         % "1.20.1"
