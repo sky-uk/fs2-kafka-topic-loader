d15bd017bc8fb7d2312df891ee26609ed5f83451
Ben Carter <ben.carter@sky.uk>
2024-09-27 14:18:39 +0100

    Add missing implicit warning

--- src/main/scala/uk/sky/fs2/kafka/topicloader/Loggable.scala
+++ src/main/scala/uk/sky/fs2/kafka/topicloader/Loggable.scala
@@ -2,17 +2,24 @@ package uk.sky.fs2.kafka.topicloader
 
 import org.typelevel.log4cats.{Logger, LoggerFactory}
 
+import scala.annotation.implicitNotFound
+
 /** Convenience trait to avoid implementing overloads for LoggerFactory and Logger. This means library users don't have
   * to provide a specific one if the rest of their code uses the other.
   */
+@implicitNotFound("""
+Implicit not found for Loggable[${F}].
+An implicit org.typelevel.log4cats.Logger[${F}] or org.typelevel.log4cats.LoggerFactory[${F}] must be in scope
+""")
 private sealed trait Loggable[F[_]] {
   def getLogger: Logger[F]
 }
 
-object Loggable {
-  inline def apply[F[_] : Loggable]: Loggable[F]          = summon[Loggable[F]]
-  private def apply[F[_]](logger: Logger[F]): Loggable[F] = new { override def getLogger: Logger[F] = logger }
+private object Loggable {
+  inline def apply[F[_] : Loggable]: Loggable[F] = summon[Loggable[F]]
 
   given fromLogger[F[_] : Logger]: Loggable[F]               = apply(Logger[F])
   given fromLoggerFactory[F[_] : LoggerFactory]: Loggable[F] = apply(LoggerFactory[F].getLogger)
+
+  inline private def apply[F[_]](logger: Logger[F]): Loggable[F] = new { val getLogger: Logger[F] = logger }
 }
